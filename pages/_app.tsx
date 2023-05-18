2e58f041863ecb0e22594f081d14c58c4fcc1599
Akhil Sarikonda <akhil.s@telco.in>
2023-05-18 13:03:04 +0100

    test

---
+++ pages/_app.tsx
@@ -0,0 +1,46 @@
+/** @format */
+
+import { AppProps } from 'next/app'
+import { NextComponentType, NextPageContext } from 'next'
+import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { arbitrum, goerli, mainnet, optimism, polygon } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+	[
+		mainnet,
+		polygon,
+		optimism,
+		arbitrum,
+		...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+	],
+	[publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+	appName: 'RainbowKit App',
+	projectId: 'YOUR_PROJECT_ID',
+	chains,
+})
+
+const wagmiConfig = createConfig({
+	autoConnect: true,
+	connectors,
+	publicClient,
+	webSocketPublicClient,
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<WagmiConfig config={wagmiConfig}>
+			<RainbowKitProvider chains={chains}>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiConfig>
+	)
+}
+
+export default MyApp
